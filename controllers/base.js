7a6e2fffc179d0e50dff08f5c62501abb6fe7f24
parkerproject <parkerproject@gmail.com>
2014-10-31 21:18:33 +0000

    fixed categories

--- controllers/base.js
+++ controllers/base.js
@@ -3,7 +3,6 @@ var collections = ['deals', 'cities', 'categories'];
 var db = require("mongojs").connect(process.env.BOXEDSALE_MONGODB_URL, collections);
 var Joi = require('joi');
 var _ = require('underscore');
-var Promise = require('es6-promise').Promise;
 var loading = 'Fetching deals...';
 
 
@@ -30,7 +29,7 @@ module.exports = {
       var findObj = {};
 
 
-      if (request.query.category) findObj.category_name = new RegExp(request.query.category, 'i');
+      if (request.query.category) findObj.category_name = [new RegExp(request.query.category, 'i')];
       if (request.query.city) findObj.merchant_locality = new RegExp(request.query.city, 'i');
       if (request.query.provider) findObj.provider_name = new RegExp(request.query.provider, 'i');
       if (request.query.region) findObj.merchant_region = new RegExp(request.query.region, 'i');
@@ -60,7 +59,7 @@ module.exports = {
   featured: {
     handler: function(request, reply) {
 
-      var providers = ['livingsocials', 'amazon local', 'groupon'];
+      var providers = ['livingsocial', 'amazon local', 'groupon'];
       var queryObj = {};
       var results = [];
       var count = providers.length;
