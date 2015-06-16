9e70c8d25b64e673cdf15cc72e9a3d21d0d01a48
parkerproject <parkerproject@gmail.com>
2015-06-16 15:54:41 +0000

    added zipcode

--- controllers/base.js
+++ controllers/base.js
@@ -10,157 +10,150 @@ var google = require('google');
 
 
 String.prototype.replaceArray = function(find, replace) {
-    var replaceString = this;
-    var regex;
-    for (var i = 0; i < find.length; i++) {
-        regex = new RegExp(find[i], "g");
-        replaceString = replaceString.replace(regex, replace[i]);
-    }
-    return replaceString;
+  var replaceString = this;
+  var regex;
+  for (var i = 0; i < find.length; i++) {
+    regex = new RegExp(find[i], "g");
+    replaceString = replaceString.replace(regex, replace[i]);
+  }
+  return replaceString;
 };
 
 
 
 var priceArray = {
-    'less than $10': {
-        $lt: 10
-    },
-    'less than $25': {
-        $lt: 25
-    },
-    'less than $50': {
-        $lt: 50
-    },
-    'less than $75': {
-        $lt: 75
-    },
-    'over $75': {
-        $gt: 75
-    }
+  'less than $10': {
+    $lt: 10
+  },
+  'less than $25': {
+    $lt: 25
+  },
+  'less than $50': {
+    $lt: 50
+  },
+  'less than $75': {
+    $lt: 75
+  },
+  'over $75': {
+    $gt: 75
+  }
 };
 
 function hashFn(item, arr) {
-    return arr[item];
+  return arr[item];
 }
 
 
 module.exports = {
-    all_deals: {
-        handler: function(request, reply) {
+  all_deals: {
+    handler: function(request, reply) {
 
-            "use strict";
+      "use strict";
 
-            var limit = request.query.limit || 20;
-            var skip = request.query.offset || 0;
-            var findObj = {};
-
-
-
-            if (request.query.category) {
-                var item = decodeURIComponent(request.query.category);
-
-                findObj.category_name = new RegExp(item, "i");
-            }
+      var limit = request.query.limit || 20;
+      var skip = request.query.offset || 0;
+      var findObj = {};
 
 
-            if (request.query.city) findObj.merchant_locality = new RegExp(request.query.city, 'i');
-            if (request.query.provider) findObj.provider_name = new RegExp(request.query.provider, 'i');
 
-            if (request.query.geo) {
-                var lng = request.query.geo[0];
-                var lat = request.query.geo[1];
+      if (request.query.category) {
+        var item = decodeURIComponent(request.query.category);
 
-                findObj.loc = {
-                    $near: {
-                        $geometry: {
-                            type: "Point",
-                            coordinates: [lng, lat]
-                        },
-                        $maxDistance: 10000.72 // 5 miles = roughly 8046.72meteres
-                    }
-                };
+        findObj.category_name = new RegExp(item, "i");
+      }
 
-            }
 
-            db.deals.find(findObj).skip(skip).limit(limit, function(err, results) {
-                reply(results);
-            });
-        },
+      if (request.query.city) findObj.merchant_locality = new RegExp(request.query.city, 'i');
+      if (request.query.provider) findObj.provider_name = new RegExp(request.query.provider, 'i');
 
-        validate: {
-            query: {
-                limit: Joi.number().integer().min(1).default(20),
-                offset: Joi.number().min(1).integer(),
-                category: Joi.string(),
-                city: Joi.string(),
-                provider: Joi.string(),
-                region: Joi.string(),
-                geo: Joi.array()
-            }
-        }
+      if (request.query.geo) {
+        var lng = request.query.geo[0];
+        var lat = request.query.geo[1];
 
-    },
-    
-        one_deal: {
-        handler: function(request, reply) {
+        findObj.loc = {
+          $near: {
+            $geometry: {
+              type: "Point",
+              coordinates: [lng, lat]
+            },
+            $maxDistance: 10000.72 // 5 miles = roughly 8046.72meteres
+          }
+        };
 
-            "use strict";
+      }
 
-            var id = request.query.id;
-
-            db.deals.findOne({deal_id: id}, function(err, results) {
-                if(err) reply('Error retrieving deal');
-                reply(results);
-            });
-           
-        },
-
-        validate: {
-            query: {
-                id: Joi.string()
-            }
-        }
-
+      db.deals.find(findObj).skip(skip).limit(limit, function(err, results) {
+        reply(results);
+      });
     },
 
-
-
-    cities: {
-        handler: function(request, reply) {
-            db.cities.find({}, function(err, results) {
-                reply(results);
-            });
-        }
-
-    },
-
-    price: {
-        handler: function(request, reply) {
-            db.price.find({}, function(err, results) {
-                reply(results);
-            });
-        }
-
-    },
-
-    categories: {
-        handler: function(request, reply) {
-            db.categories.find({}, function(err, results) {
-                reply(results);
-            });
-        }
-
-    },
-
-
-    providers: {
-        handler: function(request, reply) {
-            db.providers.find({}, function(err, results) {
-                reply(results);
-            });
-        }
-
+    validate: {
+      query: {
+        limit: Joi.number().integer().min(1).
+        default (20),
+        offset: Joi.number().min(1).integer(),
+        category: Joi.string(),
+        city: Joi.string(),
+        provider: Joi.string(),
+        region: Joi.string(),
+        geo: Joi.array(),
+        zipcode: Joi.string()
+      }
     }
+
+  },
+
+  one_deal: {
+    handler: function(request, reply) {
+
+      "use strict";
+
+      var id = request.query.id;
+
+      db.deals.findOne({
+        deal_id: id
+      }, function(err, results) {
+        if (err) reply('Error retrieving deal');
+        reply(results);
+      });
+
+    },
+
+    validate: {
+      query: {
+        id: Joi.string()
+      }
+    }
+
+  },
+
+  price: {
+    handler: function(request, reply) {
+      db.price.find({}, function(err, results) {
+        reply(results);
+      });
+    }
+
+  },
+
+  categories: {
+    handler: function(request, reply) {
+      db.categories.find({}, function(err, results) {
+        reply(results);
+      });
+    }
+
+  },
+
+
+  providers: {
+    handler: function(request, reply) {
+      db.providers.find({}, function(err, results) {
+        reply(results);
+      });
+    }
+
+  }
 
 
 };
