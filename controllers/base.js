1158e9ae6f8b07987def638b16f0b6c21198a050
Parker <parkerproject@gmail.com>
2015-06-22 17:34:46 -0400

    updates

--- controllers/base.js
+++ controllers/base.js
@@ -14,51 +14,53 @@ var async = require('async');
 // });
 
 function deal(item) {
-  return {
-    "loc": item.loc,
-    "category_name": "Food & Drinks",
-    "large_image": item.large_image,
-    "deal_id": item.deal_id,
-    "title": item.price+" Certificate",
-    "url": "http://www.anrdoezrs.net/click-7742062-10451612?url=https://www.restaurant.com/taqueria-mi-oficina-houston-pid=215258",
-    "small_image": "https://rdc.rdcimage.com:443/partner-images/215258/listingsimage_photo4.jpg",
-    "old_price": item.price,
-    "new_price": item.retailprice,
-    "expires_at": "",
-    "quantity_bought": 0,
-    "merchant_address": item.address,
-    "savings": 60,
-    "insert_date": "2015-06-06 18:40:14.322011",
-    "merchant_name": "Taqueria Mi Oficina",
-    "provider_name": "Restaurant.com",
-    "zip_code": "77007",
-    "phone": "(832)814-0311",
-    "merchant_locality": "Houston",
-    "description": ""
-  };
+    return {
+        "loc": item.loc,
+        "category_name": "Food & Drinks",
+        "large_image": item.large_image,
+        "deal_id": item.deal_id,
+        "title": item.price + " Certificate",
+        "url": item.BUYURL,
+        "small_image": item.small_image,
+        "old_price": item.price,
+        "new_price": item.retailprice,
+        "expires_at": "",
+        "quantity_bought": 0,
+        "merchant_address": item.address,
+        "savings": item.savings,
+        "insert_date": "",
+        "merchant_name": item.merchant_name,
+        "provider_name": "Restaurant.com",
+        "zip_code": item.zip_code,
+        "phone": item.phone,
+        "merchant_locality": item.merchant_locality,
+        "description": item.description,
+        "keywords": item.keywords,
+        "coupon": "no"
+    };
 }
 
 
 var q = async.queue(function(doc, callback) {
-  // code for your update
+    // code for your update
 
-  db.test.save({
-    url: doc.BUYURL
-  });
+    db.test.save({
+        url: doc.BUYURL
+    });
 
 }, Infinity);
 
 var cursor = db.cj.find({
-  PROGRAMNAME: "Restaurant.com"
+    PROGRAMNAME: "Restaurant.com"
 });
 cursor.forEach(function(err, doc) {
-  if (err) throw err;
-  if (doc) q.push(doc); // dispatching doc to async.queue
+    if (err) throw err;
+    if (doc) q.push(doc); // dispatching doc to async.queue
 });
 
 q.drain = function() {
-  if (cursor.isClosed()) {
-    console.log('all items have been processed');
-    db.close();
-  }
+    if (cursor.isClosed()) {
+        console.log('all items have been processed');
+        db.close();
+    }
 };
--- controllers/scraper.js
+++ controllers/scraper.js
@@ -3,58 +3,58 @@ var x = Xray();
 var _ = require('lodash');
 
 
-var url = 'http://www.anrdoezrs.net/click-7742062-10451612?url=http://www.restaurant.com/le-peep-evanston-pid=962?prti=1647';
+var url = 'http://www.anrdoezrs.net/click-7742062-10451612?url=http%3A%2F%2Fwww.restaurant.com%2Fmicrosite%2F650%3Fprti%3D1647';
 
 function clean(n) {
-  return n.trim();
+    return n.trim();
 }
 
 x(url, {
-  merchant: '.details h3',
-  large_image: '.mainImage img@src',
-  address: '.street',
-  meta_property: ['meta@property'],
-  meta_content: ['meta@content']
+    merchant: '.details h3',
+    large_image: '.mainImage img@src',
+    address: '.street',
+    meta_property: ['meta@property'],
+    meta_content: ['meta@content']
 })(function(err, obj) {
-//   var address = obj.address.trim();
-//   address = address.split('\r\n');
-//   address = _.map(address, clean);
-//   address = address.toString();
-//   address = address.replace(/,/g, ' ');
+    //   var address = obj.address.trim();
+    //   address = address.split('\r\n');
+    //   address = _.map(address, clean);
+    //   address = address.toString();
+    //   address = address.replace(/,/g, ' ');
+    if (err) console.log(obj);
 
-  var latIndex = obj.meta_property.indexOf('place:location:latitude');
-  var lngIndex = obj.meta_property.indexOf('place:location:longitude');
-	var phoneIndex = obj.meta_property.indexOf('restaurant:contact_info:phone_number');
-	var streetIndex = obj.meta_property.indexOf('restaurant:contact_info:street_address');
-	var localityIndex = obj.meta_property.indexOf('restaurant:contact_info:locality');
-	var regionIndex = obj.meta_property.indexOf('restaurant:contact_info:region');
-	var postalcodeIndex = obj.meta_property.indexOf('restaurant:contact_info:postal_code');
 
-	
-	
-// 	     'restaurant:contact_info:street_address',                                  
-//      'restaurant:contact_info:locality',                                        
-//      'restaurant:contact_info:region',                                          
-//      'restaurant:contact_info:postal_code',                                     
-//      'restaurant:contact_info:country_name',                                    
-//      'restaurant:contact_info:phone_number',
-
-  obj.merchant_name = obj.merchant.trim();
-  obj.category_name = "Food & Drinks";
-  obj.address = obj.meta_content[streetIndex]+ ' '+obj.meta_content[localityIndex]+ ' '+ obj.meta_content[regionIndex]+' '+obj.meta_content[postalcodeIndex];
-	obj.phone = obj.meta_content[phoneIndex];
-  obj.loc = {
-    type: "Point",
-    coordinates: [obj.meta_content[lngIndex],obj.meta_content[latIndex]]
-  };
-	obj.zip_code = obj.meta_content[postalcodeIndex];
-	obj.merchant_locality = obj.meta_content[localityIndex];
-  obj.meta_content = null;
-  obj.meta_property = null;
+    var latIndex = obj.meta_property.indexOf('place:location:latitude');
+    var lngIndex = obj.meta_property.indexOf('place:location:longitude');
+    var phoneIndex = obj.meta_property.indexOf('restaurant:contact_info:phone_number');
+    var streetIndex = obj.meta_property.indexOf('restaurant:contact_info:street_address');
+    var localityIndex = obj.meta_property.indexOf('restaurant:contact_info:locality');
+    var regionIndex = obj.meta_property.indexOf('restaurant:contact_info:region');
+    var postalcodeIndex = obj.meta_property.indexOf('restaurant:contact_info:postal_code');
 
 
 
+    // 	     'restaurant:contact_info:street_address',                                  
+    //      'restaurant:contact_info:locality',                                        
+    //      'restaurant:contact_info:region',                                          
+    //      'restaurant:contact_info:postal_code',                                     
+    //      'restaurant:contact_info:country_name',                                    
+    //      'restaurant:contact_info:phone_number',
 
-  console.log(obj);
+    obj.merchant_name = obj.merchant.trim();
+    obj.category_name = "Food & Drinks";
+    obj.address = obj.meta_content[streetIndex] + ' ' + obj.meta_content[localityIndex] + ' ' + obj.meta_content[regionIndex] + ' ' + obj.meta_content[postalcodeIndex];
+    obj.phone = obj.meta_content[phoneIndex];
+    obj.loc = {
+        type: "Point",
+        coordinates: [obj.meta_content[lngIndex], obj.meta_content[latIndex]]
+    };
+    obj.zip_code = obj.meta_content[postalcodeIndex];
+    obj.merchant_locality = obj.meta_content[localityIndex];
+    obj.meta_content = null;
+    obj.meta_property = null;
+
+
+    //console.log(obj);
 
 });
